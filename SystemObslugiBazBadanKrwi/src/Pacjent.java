6f003f21036db8ede616cf42ae5e24b2b6464d8c
krakaleksandra <krakaleksandra@gmail.com>
2020-12-29 21:47:25 +0100

    atrybuty, oraz get i set

--- SystemObslugiBazBadanKrwi/src/Pacjent.java
+++ SystemObslugiBazBadanKrwi/src/Pacjent.java
@@ -1,5 +1,48 @@
-public class Pacjent {
-    string imie;
-    
+public class Pacjent{
 
+    private int pesel;
+    private String nazwisko;
+    private String imie;
+    private String plec;
+    private int wiek;
+
+    public int getPesel() {
+        return pesel;
+    }
+
+    public void setPesel(int pesel) {
+        this.pesel = pesel;
+    }
+
+    public String getNazwisko() {
+        return nazwisko;
+    }
+
+    public void setNazwisko(String nazwisko) {
+        this.nazwisko = nazwisko;
+    }
+
+    public String getImie() {
+        return imie;
+    }
+
+    public void setImie(String imie) {
+        this.imie = imie;
+    }
+
+    public String getPlec() {
+        return plec;
+    }
+
+    public void setPlec(String plec) {
+        this.plec = plec;
+    }
+
+    public int getWiek() {
+        return wiek;
+    }
+
+    public void setWiek(int wiek) {
+        this.wiek = wiek;
+    }
 }
