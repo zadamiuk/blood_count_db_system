a4a15844ca41d3f89ff5fadcd684e3b44579cf8b
krakaleksandra <krakaleksandra@gmail.com>
2021-01-06 15:29:22 +0100

    robie od poczatku

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import javax.swing.SwingUtilities;
+
+/** Klasa startująca aplikacje
+ *
+ * @author AZ_KA
+ */
+public class Main {
+    /**
+     * Metoda glowna
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
